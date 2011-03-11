565f4a6608c089b6cd24114d08186b8924051f58
Nicolás Hock Isaza <nhocki@gmail.com>
2011-03-10 23:43:17 -0500

    Started some readme stuff

--- lib/reminder/notifier.rb
+++ lib/reminder/notifier.rb
@@ -1,7 +1,25 @@
+
 module Reminder
   class Notifier
-    def self.run options
+    
+    def initialize options, output = STDOUT
+      @message   = options[:message]
+      @title     = options[:title]
+      @time      = options[:time]
+    end
+    
+    def run 
       raise GrowlNotInstalled and return unless Growl.installed?
+      notification = Growl.new
+      notification.message = @message
+      notification.title = @title
+      notification.icon = nil
+      
+      while true
+        notification.run
+        sleep(@time * 60)
+      end
+            
     end
     
   end
--- spec/reminder/notifier_spec.rb
+++ spec/reminder/notifier_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 module Reminder
   describe Notifier do
+    let(:output) {double('output').as_null_object}
     context "Growl is not installed" do
       it "raises an exception" do
         Growl.stub!(:installed?).and_return(false)
         expect{
-          Notifier.run(default_options)
+          Notifier.new(default_options, output).run
         }.to raise_error(
           Reminder::GrowlNotInstalled, /Growl is not installed/
         )
@@ -15,6 +16,10 @@ module Reminder
   
     context "Growl is happily installed" do
       
+      it "should show notifications instantly" do
+        notifier = Notifier.new(default_options, output)
+        notifier.run
+      end
     end
   
   end
